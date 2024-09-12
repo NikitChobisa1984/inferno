0d6f27402aac0aac11e3091cdccbcd6740a034eb
Tyger <nikitchobisa18@gmail.com>
2024-09-12 21:44:52 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,14 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase
 import { getAuth } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDhn51-a_k3jGFgt1pWf_fNnr3gAd02cCM",
+  authDomain: "inferno---the-secret-syndicate.firebaseapp.com",
+  databaseURL: "https://inferno---the-secret-syndicate-default-rtdb.firebaseio.com",
+  projectId: "inferno---the-secret-syndicate",
+  storageBucket: "inferno---the-secret-syndicate.appspot.com",
+  messagingSenderId: "87319334121",
+  appId: "1:87319334121:web:272b2c1e3f2f4d29447478",
+  measurementId: "G-V920H01CTQ"
 };
 
 const app = initializeApp(firebaseConfig);
